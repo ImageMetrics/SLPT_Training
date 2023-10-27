4f928589bdfd94652a9425b441ce12c8038a7fd4
MikeR <mike.rogers@image-metrics.com>
2023-10-27 15:34:02 -0400

    supporting starts in data loader - passing starts to model

--- Dataloader/WFLW_loader.py
+++ Dataloader/WFLW_loader.py
@@ -248,12 +248,134 @@ class WFLWCal_Dataset(WFLW_Dataset):
             temp_name = os.path.join(self.root, self.image_dir, temp_info[-1])
             Data_base.append({'Img': temp_name,
                               'bbox': temp_box,
-                              'point': point_coord})
+                              'point': point_coord,})
+
+        return Data_base
+
+    def get_file_information(self):
+        Data_base = []
+
+        with open(self.annotation_file) as f:
+            info_list = f.read().splitlines()
+            f.close()
+
+        for temp_info in info_list:
+            temp_point = []
+            temp_info = temp_info.split(' ')
+            for i in range(2 * self.number_landmarks):
+                temp_point.append(float(temp_info[i]))
+            point_coord = np.array(temp_point, dtype=np.float).reshape(self.number_landmarks, 2)
+            max_index = np.max(point_coord, axis=0)
+            min_index = np.min(point_coord, axis=0)
+            temp_box = np.array([min_index[0], min_index[1], max_index[0] - min_index[0],
+                                 max_index[1] - min_index[1]])
+            temp_name = os.path.join(self.root, self.image_dir, temp_info[-1])
+            temp_point = []
+            for i in range(2 * self.number_landmarks, 4 * self.number_landmarks):
+                temp_point.append(float(temp_info[i]))
+            start_coord = np.array(temp_point, dtype=np.float).reshape(self.number_landmarks, 2)
+            Data_base.append({'Img': temp_name,
+                              'bbox': temp_box,
+                              'point': point_coord,
+                              'start': start_coord,})
 
         return Data_base
 
     def __getitem__(self, idx):
-        input, meta = super().__getitem__(idx)
+        db_slic = copy.deepcopy(self.database[idx])
+        Img_path = db_slic['Img']
+        BBox = db_slic['bbox']
+        Points = db_slic['point']
+        StartPoints = db_slic['start']
+        Annotated_Points = Points.copy()
+
+        Img = cv2.imread(Img_path)
+
+        Img_shape = Img.shape
+        Img = cv2.cvtColor(Img, cv2.COLOR_RGB2BGR)
+        if len(Img_shape) < 3:
+            Img = cv2.cvtColor(Img, cv2.COLOR_GRAY2RGB)
+        else:
+            if Img_shape[2] == 4:
+                Img = cv2.cvtColor(Img, cv2.COLOR_RGBA2RGB)
+            elif Img_shape[2] == 1:
+                Img = cv2.cvtColor(Img, cv2.COLOR_GRAY2RGB)
+
+        if self.is_train == True:
+            Rotation_Factor = self.Rotation_Factor * np.pi / 180.0
+            Scale_Factor = self.Scale_Factor
+            Translation_X_Factor = self.Translation_Factor
+            Translation_Y_Factor = self.Translation_Factor
+
+            angle = np.clip(np.random.normal(0, Rotation_Factor), -2 * Rotation_Factor, 2 * Rotation_Factor)
+            Scale = np.clip(np.random.normal(self.Fraction, Scale_Factor), self.Fraction - Scale_Factor, self.Fraction + Scale_Factor)
+
+            Translation_X = np.clip(np.random.normal(0, Translation_X_Factor), -Translation_X_Factor, Translation_X_Factor)
+            Translation_Y = np.clip(np.random.normal(0, Translation_Y_Factor), -Translation_Y_Factor, Translation_Y_Factor)
+
+            trans = utils.get_transforms(BBox, Scale, angle, self.Image_size, shift_factor=[Translation_X, Translation_Y])
+
+            input = cv2.warpAffine(Img, trans, (int(self.Image_size), int(self.Image_size)), flags=cv2.INTER_LINEAR)
+
+            for i in range(self.number_landmarks):
+                Points[i,0:2] = utils.affine_transform(Points[i,0:2], trans)
+                StartPoints[i,0:2] = utils.affine_transform(StartPoints[i,0:2], trans)
+
+            if self.Flip is True:
+                Flip_Flag = np.random.randint(0, 2)
+                if Flip_Flag == 1:
+                    input, Points, StartPoints = self.Image_Flip(input, Points, StartPoints)
+
+            if self.Transfer is True:
+                Transfer_Flag = np.random.randint(0, 5)
+                input = self.Channel_Transfer(input, Transfer_Flag)
+
+            if self.Occlusion is True:
+                Occlusion_Flag = np.random.randint(0, 2)
+                if Occlusion_Flag == 1:
+                    input = self.Create_Occlusion(input)
+
+            if self.Transform is not None:
+                input = self.Transform(input)
+
+            meta = {'Img_path': Img_path,
+                    'Points': Points / (self.Image_size),
+                    'StartPoints': StartPoints / (self.Image_size),
+                    'BBox': BBox,
+                    'trans': trans,
+                    'Scale': Scale,
+                    'angle': angle,
+                    'Translation': [Translation_X, Translation_Y]}
+
+        else:
+            trans = utils.get_transforms(BBox, self.Fraction, 0.0, self.Image_size, shift_factor=[0.0, 0.0])
+
+            input = cv2.warpAffine(Img, trans, (int(self.Image_size), int(self.Image_size)), flags=cv2.INTER_LINEAR)
+
+            for i in range(self.number_landmarks):
+                Points[i, 0:2] = utils.affine_transform(Points[i, 0:2], trans)
+                StartPoints[i, 0:2] = utils.affine_transform(StartPoints[i, 0:2], trans)
+
+            meta = {
+                "Annotated_Points": Annotated_Points,
+                'Img_path': Img_path,
+                'Points': Points / (self.Image_size),
+                'StartPoints': StartPoints / (self.Image_size),
+                'BBox': BBox,
+                'trans': trans,
+                'Scale': self.Fraction,
+                'angle': 0.0,
+                'Translation': [0.0, 0.0],
+            }
+
+            # target = np.zeros((self.number_landmarks, self.Heatmap_size, self.Heatmap_size))
+            # tpts = Points / (self.Image_size - 1) * (self.Heatmap_size - 1)
+            # for i in range(self.number_landmarks):
+            #     if tpts[i, 1] > 0:
+            #         target[i] = generate_target(target[i], tpts[i], self.sigma)
+
+            if self.Transform is not None:
+                input = self.Transform(input)
 
         tmp = self.database
         is_train = self.is_train
@@ -264,3 +386,13 @@ class WFLWCal_Dataset(WFLW_Dataset):
         self.is_train = is_train
 
         return input, input_cal, meta, meta_cal
+
+    def Image_Flip(self, Img, GT, ST):
+        Mirror_GT = []
+        Mirror_ST = []
+        width = Img.shape[1]
+        for i in self.flip_index:
+            Mirror_GT.append([width - 1 - GT[i][0], GT[i][1]])
+            Mirror_ST.append([width - 1 - ST[i][0], ST[i][1]])
+        Img = cv2.flip(Img, 1)
+        return Img, numpy.array(Mirror_GT), numpy.array(Mirror_ST)
--- model.py
+++ model.py
@@ -232,3 +232,101 @@ class Sparse_alignment_network_cal(Sparse_alignment_network):
                                          self.d_model).permute(0, 3, 2, 1)
 
             return ROI_feature_3, bbox_size_3, start_anchor_3
+
+
+class Sparse_alignment_network_refine(Sparse_alignment_network_cal):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        # ROI_creator - make these a lot tighter
+        self.ROI_1 = get_roi(self.Sample_num, 2.0, 64)
+        self.ROI_2 = get_roi(self.Sample_num, 1.0, 64)
+        self.ROI_3 = get_roi(self.Sample_num, 0.5, 64)
+
+    def forward(self, image, start_landmarks, cal_image, cal_landmarks):
+
+        bs = image.size(0)
+
+        output_list = []
+
+        feature_map = self.backbone(image)
+        calibration_feature_map = self.backbone(cal_image)
+
+        # cal features
+        ROI_feature_cal_1, _, _ = self.get_image_features(calibration_feature_map, cal_landmarks, stage=1)
+        ROI_feature_cal_2, _, _ = self.get_image_features(calibration_feature_map, cal_landmarks, stage=2)
+        ROI_feature_cal_3, _, _ = self.get_image_features(calibration_feature_map, cal_landmarks, stage=3)
+
+        initial_landmarks = start_landmarks.to(image.device)
+
+        # stage_1
+        ROI_feature_1, bbox_size_1, start_anchor_1 = \
+            self.get_image_features(feature_map, initial_landmarks, stage=1)
+
+        transformer_feature_1 = self.feature_extractor(ROI_feature_1).view(bs, self.num_point, self.d_model)
+        transformer_feature_cal_1 = self.feature_extractor_cal(ROI_feature_cal_1).view(bs, self.num_point, self.d_model)
+
+        offset_1 = self.Transformer(transformer_feature_1, transformer_feature_cal_1)
+        offset_1 = self.out_layer(offset_1)
+
+        landmarks_1 = start_anchor_1.unsqueeze(1) + bbox_size_1.unsqueeze(1) * offset_1
+        output_list.append(landmarks_1)
+
+        # stage_2
+        ROI_feature_2, bbox_size_2, start_anchor_2 = \
+            self.get_image_features(feature_map, landmarks_1[:, -1, :, :], stage=2)
+
+        transformer_feature_2 = self.feature_extractor(ROI_feature_2).view(bs, self.num_point, self.d_model)
+        transformer_feature_cal_2 = self.feature_extractor_cal(ROI_feature_cal_2).view(bs, self.num_point, self.d_model)
+
+        offset_2 = self.Transformer(transformer_feature_2, transformer_feature_cal_2)
+        offset_2 = self.out_layer(offset_2)
+
+        landmarks_2 = start_anchor_2.unsqueeze(1) + bbox_size_2.unsqueeze(1) * offset_2
+        output_list.append(landmarks_2)
+
+        # stage_3
+        ROI_feature_3, bbox_size_3, start_anchor_3 = \
+            self.get_image_features(feature_map, landmarks_2[:, -1, :, :], stage=3)
+
+        transformer_feature_3 = self.feature_extractor(ROI_feature_3).view(bs, self.num_point, self.d_model)
+        transformer_feature_cal_3 = self.feature_extractor_cal(ROI_feature_cal_3).view(bs, self.num_point, self.d_model)
+
+        offset_3 = self.Transformer(transformer_feature_3, transformer_feature_cal_3)
+        offset_3 = self.out_layer(offset_3)
+
+        landmarks_3 = start_anchor_3.unsqueeze(1) + bbox_size_3.unsqueeze(1) * offset_3
+        output_list.append(landmarks_3)
+
+        return output_list
+
+    def get_image_features(self, feature_map, landmarks, stage=1):
+        bs = feature_map.size(0)
+
+        # features
+        if stage == 1:
+            ROI_anchor_1, bbox_size_1, start_anchor_1 = self.ROI_1(landmarks.detach())
+            ROI_anchor_1 = ROI_anchor_1.view(bs, self.num_point * self.Sample_num * self.Sample_num, 2)
+            ROI_feature_1 = self.interpolation(feature_map, ROI_anchor_1.detach()).view(bs, self.num_point, self.Sample_num,
+                                                                                self.Sample_num, self.d_model)
+            ROI_feature_1 = ROI_feature_1.view(bs * self.num_point, self.Sample_num, self.Sample_num,
+                                         self.d_model).permute(0, 3, 2, 1)
+            return ROI_feature_1, bbox_size_1, start_anchor_1
+
+        elif stage == 2:
+            ROI_anchor_2, bbox_size_2, start_anchor_2 = self.ROI_2(landmarks.detach())
+            ROI_anchor_2 = ROI_anchor_2.view(bs, self.num_point * self.Sample_num * self.Sample_num, 2)
+            ROI_feature_2 = self.interpolation(feature_map, ROI_anchor_2.detach()).view(bs, self.num_point, self.Sample_num,
+                                                                                self.Sample_num, self.d_model)
+            ROI_feature_2 = ROI_feature_2.view(bs * self.num_point, self.Sample_num, self.Sample_num,
+                                         self.d_model).permute(0, 3, 2, 1)
+            return ROI_feature_2, bbox_size_2, start_anchor_2
+        else:
+            ROI_anchor_3, bbox_size_3, start_anchor_3 = self.ROI_3(landmarks.detach())
+            ROI_anchor_3 = ROI_anchor_3.view(bs, self.num_point * self.Sample_num * self.Sample_num, 2)
+            ROI_feature_3 = self.interpolation(feature_map, ROI_anchor_3.detach()).view(bs, self.num_point, self.Sample_num,
+                                                                                self.Sample_num, self.d_model)
+            ROI_feature_3 = ROI_feature_3.view(bs * self.num_point, self.Sample_num, self.Sample_num,
+                                         self.d_model).permute(0, 3, 2, 1)
+
+            return ROI_feature_3, bbox_size_3, start_anchor_3
